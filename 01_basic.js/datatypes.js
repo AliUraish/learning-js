63a9480987b79ac795b9c0e9117f6d78b30799d4
Ali Uraish <aliuraishmirani@gmail.com>
2025-07-03 07:49:23 +0000

    datatypes and variables

---
+++ 01_basic.js/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs rather than browser version
+
+console.log(3
+    +
+    3) // code readability should be high 
+
+    console.log("Ali");
+    
+
+    let name = "Ali"
+    let age = 18
+    let isloggedin = true
+    let state;
+
+    // number => 2 to power 53 
+    // bigint
+    // string==>""
+    // boolean ==> true/false
+    // null ==> standalone value
+    // undefined ==>
+    // symbol ==> unique
+    
+
+    // object
+
+    console.log(typeof age)
---
+++ 01_basic.js/variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "ali@google.com"
+var accountPassword = "121542"
+accountCity = "kar"
+let accountState;
+//accountId = 2
+
+
+
+accountEmail = "mah@google.com"
+accountPassword = "125346"
+accountCity = "lah"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
