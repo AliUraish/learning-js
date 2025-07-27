f96bbb34c849a095be0ae3946c87d391c8bb101d
AliUraish <aliuraishmirani@gmail.com>
2025-07-28 02:58:41 +0500

    operations start

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -19,7 +19,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true
 //0 => false
@@ -29,9 +29,36 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 //33 => "33"
 //true => "true"
 //false => "false"
+
+//******************Operations******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3); //power
+//console.log(2/3);
+//console.log(2%3); //remainder
+
+let str1 = "hello"
+let str2 = " Aliuraish"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+let str4 = "1"
+let str5 = "2"
+
+let str6 = str4 + str5
+console.log(str6);
+
+let str7 = str4 + 2
